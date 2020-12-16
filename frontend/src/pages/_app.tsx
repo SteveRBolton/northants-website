4edcb8926957d8ffaf011e3ad9038c4c09b2878a
Lee Mills <leem@deeson.co.uk>
2020-12-16 09:11:04 +0000

    Updated frontend Platform.sh config

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -10,9 +10,11 @@ import '../css/reset.css';
 import { initializeApollo } from '../lib/apolloClient';
 
 function NorthantsApp({ Component, pageProps, router, globals }: AppProps & GetCMSGlobals): ReactElement {
-  const theme = process.env.NEXT_PUBLIC_THEME
-    ? { north: north_theme, west: west_theme, gds: GDS_theme }[process.env.NEXT_PUBLIC_THEME]
-    : GDS_theme;
+  // const theme = process.env.NEXT_PUBLIC_THEME
+  //   ? { north: north_theme, west: west_theme, gds: GDS_theme }[process.env.NEXT_PUBLIC_THEME]
+  //   : GDS_theme;
+
+  const theme = north_theme;
 
   const isHomePage = router.pathname === '/';
 
