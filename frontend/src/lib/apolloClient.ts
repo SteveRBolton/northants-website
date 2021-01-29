5d950cfab26cf050a8ef09e6df94ec1ee77f86d9
Lee Mills <leem@deeson.co.uk>
2021-01-29 13:52:12 +0000

    Test hardcoded graphql link

--- frontend/src/lib/apolloClient.ts
+++ frontend/src/lib/apolloClient.ts
@@ -10,7 +10,7 @@ function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
-      uri: 'https://beta.develop-sr3snxi-nb6dpqmudzbik.uk-1.platformsh.site/api/graphql', // Server URL (must be absolute)
+      uri: 'https://beta.develop-sr3snxi-buviaip2abrdc.uk-1.platformsh.site/api/graphql', // Server URL (must be absolute)
       credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
     }),
     cache: new InMemoryCache({}),
