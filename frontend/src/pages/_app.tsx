304838fcc7dc48029f29769cbad40fadaf7c28ae
Lee Mills <leem@deeson.co.uk>
2021-02-15 12:44:18 +0000

    Change GTM code

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -34,11 +34,12 @@ function NorthantsApp({
     if (document.cookie.includes('"cookiesAccepted":true')) {
       if (gtm) {
         const tag = document.createElement('script');
-        tag.src = `https://www.googletagmanager.com/gtag/js?id=${gtm}`;
+        tag.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','${gtm}');`;
         document.getElementsByTagName('head')[0].appendChild(tag);
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push('js', new Date());
-        window.dataLayer.push('config', `${gtm}`);
       }
     }
   }, []);
