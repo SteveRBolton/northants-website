b6474c3607a7431b25c09a3255d1c0c2d954ed64
Lee Mills <leem@deeson.co.uk>
2021-02-23 07:53:40 +0000

    Sync GTM code with develop

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -34,12 +34,11 @@ function NorthantsApp({
     if (document.cookie.includes('"cookiesAccepted":true')) {
       if (gtm) {
         const tag = document.createElement('script');
-        tag.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer','${gtm}');`;
+        tag.src = `https://www.googletagmanager.com/gtag/js?id=${gtm}`;
         document.getElementsByTagName('head')[0].appendChild(tag);
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push('js', new Date());
+        window.dataLayer.push('config', `${gtm}`);
       }
     }
   }, []);
