93bbfbfd5476a6d2bfd4f58dca6ae6e8872e2587
Lee Mills <leem@deeson.co.uk>
2021-03-12 10:11:43 +0000

    Update URL

--- frontend/src/cmsPages/ErrorPage/index.tsx
+++ frontend/src/cmsPages/ErrorPage/index.tsx
@@ -22,7 +22,7 @@ export default function ErrorPage({ pageTitle, errorCode }: ErrorPageProps): Rea
           <p>If you entered a web address, check it is correct.</p>
 
           <p>
-            You can <a href="#"> see a list of council services on the homepage </a> or use the search box above to find
+            You can <a href="/"> see a list of council services on the homepage </a> or use the search box above to find
             the information you need.
           </p>
 
