8d2968c7cb95484ce45360204efd3f6b6505abe7
amyhaslam <amyh@deeson.co.uk>
2021-04-21 10:16:19 +0100

    [NOR-124-backend] fixed highlight backend error

--- backend/src/modules/nc_system/src/Entity/Paragraph/Highlight.php
+++ backend/src/modules/nc_system/src/Entity/Paragraph/Highlight.php
@@ -6,33 +6,28 @@ namespace Drupal\nc_system\Entity\Paragraph;
 
 use Drupal\nc_system\Entity\GraphQLEntityFieldResolver;
 use Drupal\paragraphs\Entity\Paragraph;
-use Drupal\search_api\Plugin\DataType\Text;
+use Drupal\text\Plugin\Field\FieldType\TextItemBase;
 use Drupal\views\Plugin\views\field\Boolean;
 
 class Highlight extends Paragraph implements GraphQLEntityFieldResolver {
-
   public function getTitle(): string {
-
     $title = $this->get('field_highlight_title');
-    $item =  $title->first()->getString();
+    $item = $title->getString();
     return $item;
 
   }
-
-  public function getContent(): ?string{
+  public function getContent(): ?TextItemBase {
     $content = $this->get('field_content');
 
-    $item = $content->first()->getString();
+    $item = $content->first();
 
     return $item;
 
   }
   public function getWarning(): bool {
     $isWarning = $this->get('field_is_warning');
-    $item = $isWarning->getBoolean;
-
+    $item = $isWarning->getString() === "1";
     return $item;
-
   }
   /**
    * @param string $fieldName
@@ -41,11 +36,9 @@ class Highlight extends Paragraph implements GraphQLEntityFieldResolver {
    * @throws \Exception
    */
   public function resolveGraphQLFieldToValue(string $fieldName) {
-
     if($fieldName === "title") {
       return $this->getTitle();
     }
-
     if($fieldName === "content") {
       return $this->getContent();
     }
