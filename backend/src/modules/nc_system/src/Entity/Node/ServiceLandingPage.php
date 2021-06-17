c702af7ed6455819e94b775c5ba6f959270acc3e
Lee Mills <leem@deeson.co.uk>
2021-06-17 14:50:06 +0100

    Fix an on page service level not being set causing notices in the error log

--- backend/src/modules/nc_system/src/Entity/Node/ServiceLandingPage.php
+++ backend/src/modules/nc_system/src/Entity/Node/ServiceLandingPage.php
@@ -75,10 +75,10 @@ class ServiceLandingPage extends Content implements GraphQLEntityFieldResolver {
    * This function returns an array containing information about Service Alerts
    */
   public function getServicePageAlerts() {
-    $active = $this->get('field_enable_alert')->getValue()[0]['value'];
+    $active = empty($this->get('field_enable_alert')->getValue()) ? FALSE: $this->get('field_enable_alert')->getValue()[0]['value'];
     $expired = false;
 
-    $expiration = $this->get('field_alert_expiration_date')->getValue()[0]['value'];
+    $expiration = empty($this->get('field_alert_expiration_date')->getValue()) ? FALSE : $this->get('field_alert_expiration_date')->getValue()[0]['value'];
     $alertItem = ['title' => ''];
 
     if($expiration) {
