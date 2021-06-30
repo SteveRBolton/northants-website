cf54fd7236926d12a620bfcf49b04f7ad584b57e
William Wise <williamwise@Williams-MacBook-Pro.local>
2021-06-30 11:19:35 +0100

    adding config for memorial styling

--- frontend/src/cmsPages/MemorialHomepage/index.tsx
+++ frontend/src/cmsPages/MemorialHomepage/index.tsx
@@ -1,3 +1,4 @@
+import App, { AppProps, AppContext, AppInitialProps } from 'next/app';
 import {
   MaxWidthContainer,
   PageMain,
@@ -5,14 +6,53 @@ import {
   PromoBanner,
   Breadcrumbs,
   PhaseBanner,
+  north_theme,
+  GDS_theme,
+  west_theme,
+  lb_theme_north,
+  lb_theme_west,
+  Header,
+  PageWrapper,
+  MemorialHero,
+  NewsArticleFeaturedBlock,
+  PromotedLinks,
 } from 'northants-design-system';
 import React, { ReactElement } from 'react';
 import Head from 'next/head';
 import { PageLinkProp } from 'northants-design-system/build/library/structure/ServicesLinksList/ServicesLinksList.types';
 
 import { PromoBannerProps } from 'northants-design-system/build/library/structure/PromoBanner/PromoBanner.types';
+import { ThemeProvider } from 'styled-components';
+
+import { initializeApollo } from '../../lib/apolloClient';
 import TextWithSlices, { TextWithSlicesProps } from '../../components/TextWithSlices';
 import AlertBannerServiceIE from '../../components/AlertBannerService';
+import { GetCMSGlobals } from '../../api/graphql/__generated__/GetCMSGlobals';
+import { getCMSGlobals } from '../../api/graphql/queries';
+
+// unsure if needed
+declare global {
+  interface Window {
+    dataLayer: [
+      {
+        event: string;
+      }
+    ];
+  }
+}
+enum Theme {
+  North = 'north',
+  North_LB = 'lb_theme_north',
+  West = 'west',
+  West_LB = 'lb_theme_west',
+  GDS = 'gds',
+}
+
+enum Website {
+  North = 'north',
+  West = 'west',
+  GDS = 'gds',
+}
 
 type MemorialHomepageProps = {
   body?: TextWithSlicesProps;
@@ -24,7 +64,7 @@ type MemorialHomepageProps = {
   promoBody?: TextWithSlicesProps;
 };
 
-export default function MemorialHomepage({
+function MemorialHomepage({
   body,
   serviceLinks,
   metaTitle,
@@ -32,7 +72,139 @@ export default function MemorialHomepage({
   metaKeywords,
   promoBanner,
   promoBody,
-}: MemorialHomepageProps): ReactElement {
+  globals,
+  theme,
+}: AppProps &
+  MemorialHomepageProps &
+  GetCMSGlobals & {
+    theme: Theme;
+    website: Website;
+  }): ReactElement {
+  let actualThemeObject;
+  console.log(theme);
+  console.log(Theme);
+  switch (theme) {
+    case Theme.North:
+      actualThemeObject = north_theme;
+      break;
+    case Theme.West:
+      actualThemeObject = west_theme;
+      break;
+    case Theme.North_LB:
+      actualThemeObject = lb_theme_north;
+      break;
+    case Theme.West_LB:
+      actualThemeObject = lb_theme_west;
+      break;
+    default:
+      actualThemeObject = GDS_theme;
+  }
+  const newsArticleData = {
+    articles: [
+      {
+        id: '6036694e95f7c8c6729a46bf',
+        url: '/',
+        title: 'Veniam nostrud excepteur elit non cupidatat deserunt sunt amet ad ipsum sunt incididunt.',
+        date: 1614178638,
+        image720x405:
+          'https://core-cms.bfi.org.uk/sites/default/files/styles/responsive/public/1440/810/1/2020-08/bfi-film-academy-2020-recruitment-campaign-watershed-two-girls-behind-camera.jpeg',
+        image72x41:
+          'https://core-cms.bfi.org.uk/sites/default/files/styles/responsive/public/144/81/1/2020-08/bfi-film-academy-2020-recruitment-campaign-watershed-two-girls-behind-camera.jpeg',
+        imageAltText: 'The alt text',
+      },
+      {
+        id: '6036694e71b8eedb00cff39f',
+        url: '/',
+        title: 'Non minim ad ullamco exercitation pariatur eu dolor occaecat ullamco culpa excepteur cillum irure.',
+        date: 1614178638,
+        image720x405:
+          'https://core-cms.bfi.org.uk/sites/default/files/styles/responsive/public/1440/810/1/2020-08/bfi-film-academy-2020-recruitment-campaign-watershed-two-girls-behind-camera.jpeg',
+        image72x41:
+          'https://core-cms.bfi.org.uk/sites/default/files/styles/responsive/public/144/81/1/2020-08/bfi-film-academy-2020-recruitment-campaign-watershed-two-girls-behind-camera.jpeg',
+        imageAltText: 'The alt text',
+      },
+      {
+        id: '6036694ed3c1de9114024a02',
+        url: '/',
+        title: 'In sint incididunt dolor officia consectetur proident mollit exercitation magna.',
+        excerpt:
+          'Cillum occaecat eiusmod pariatur cillum Lorem sunt pariatur proident aliquip pariatur aute nostrud. Veniam aliqua qui id consectetur sit incididunt. Sint non voluptate adipisicing anim. Amet tempor id in adipisicing sunt. Aliquip dolore ipsum occaecat officia anim aliqua minim consequat Lorem ipsum.\r\n',
+        date: 1614178638,
+        image720x405:
+          'https://core-cms.bfi.org.uk/sites/default/files/styles/responsive/public/1440/810/1/2020-08/bfi-film-academy-2020-recruitment-campaign-watershed-two-girls-behind-camera.jpeg',
+        image72x41:
+          'https://core-cms.bfi.org.uk/sites/default/files/styles/responsive/public/144/81/1/2020-08/bfi-film-academy-2020-recruitment-campaign-watershed-two-girls-behind-camera.jpeg',
+        imageAltText: 'The alt text',
+      },
+    ],
+  };
+  const serviceLinksArray = [
+    {
+      title: 'Condolence book',
+      url: '/',
+      iconKey: 'covid',
+      quickLinksArray: [
+        {
+          title: 'Sign the condolence book and leave a tribute',
+          url: '/',
+        },
+      ],
+    },
+  ];
+  const memorialServiceLinksArray = [
+    {
+      title: 'The Royal Website',
+      url: '/',
+      iconKey: 'covid',
+      quickLinksArray: [
+        {
+          title: 'Details and announcements of the funeral of ...',
+          url: '/',
+        },
+      ],
+    },
+    {
+      title: 'Guidance for the Period of National Mourning',
+      url: '/',
+      iconKey: 'covid',
+      quickLinksArray: [
+        {
+          title: 'Information and key links regarding national mourning',
+          url: '/',
+        },
+      ],
+    },
+    {
+      title: 'The Royal Website',
+      url: '/',
+      iconKey: 'covid',
+      quickLinksArray: [
+        {
+          title: 'Details and announcements of the funeral of ...',
+          url: '/',
+        },
+      ],
+    },
+  ];
+  const ExampleMemorialHeroArgs = {
+    src: 'http://placehold.it/1000x700',
+    placeholder: 'http://placehold.it/250x175',
+    alt: 'Image showing ...',
+    children: <ServicesLinksList hasBackground hideHeader serviceLinksArray={serviceLinksArray} oneCol />,
+    councilServices: (
+      <PromotedLinks
+        oneCol
+        promotedLinksArray={[
+          {
+            title: 'Proceed to Council services',
+            url: '/',
+          },
+        ]}
+      />
+    ),
+    theme: actualThemeObject,
+  };
+
   return (
     <>
       <Head>
@@ -49,6 +221,17 @@ export default function MemorialHomepage({
         <meta property="twitter:description" content={metaDescription || ''} />
       </Head>
       <div>&nbsp;</div>
+      <ThemeProvider theme={actualThemeObject}>
+        <Header />
+        <MemorialHero {...ExampleMemorialHeroArgs} />
+
+        <PageWrapper colour="grey_light">
+          <MaxWidthContainer noBackground>
+            <ServicesLinksList hideHeader serviceLinksArray={memorialServiceLinksArray} />
+            <NewsArticleFeaturedBlock articles={newsArticleData.articles} viewAllLink="/news/" />
+          </MaxWidthContainer>
+        </PageWrapper>
+      </ThemeProvider>
       <MaxWidthContainer>
         <PhaseBanner />
         <Breadcrumbs
@@ -79,3 +262,38 @@ export default function MemorialHomepage({
     </>
   );
 }
+MemorialHomepage.getInitialProps = async (
+  appContext: AppContext
+): Promise<AppInitialProps & GetCMSGlobals & { theme: Theme; website: Website }> => {
+  const client = initializeApollo();
+  console.log('CALLED');
+  // Get globals
+  const globals = await client
+    .query<GetCMSGlobals>({
+      query: getCMSGlobals,
+      variables: {},
+    })
+    .then((res) => {
+      console.error('HERE');
+      console.log(res.data);
+      return res.data.globals;
+    });
+
+  // set cache for use with Cloudflare
+  if (process.env.NODE_ENV !== 'development') {
+    if (appContext.ctx && appContext.ctx.res) {
+      appContext.ctx.res.setHeader('Cache-Control', 'public, max-age=3600');
+    }
+  }
+
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+  return {
+    ...appProps,
+    globals,
+    theme: process.env.NEXT_PUBLIC_THEME as Theme,
+    website: process.env.NEXT_PUBLIC_WEBSITE as Website,
+  };
+};
+
+export default MemorialHomepage;
